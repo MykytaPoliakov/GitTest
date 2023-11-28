4cd91ea09c745eda1f994acafa47c626cf90d879
MykytaPoliakov <Pola_dl02@student.itstep.org>
2023-11-28 12:33:16 +0200

    Add function which print "Hello, Antonio"

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def print_hi(name):
     print(f'Hi, {name}')
 
+def print_hi_Antonio():
+    print("Hello, Antonio")
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hi_Antonio()
+    
